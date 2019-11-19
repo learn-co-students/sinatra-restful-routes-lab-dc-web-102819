4312964c97086ba649e981fd3745de41842fed17
github username <github email address>
2019-11-19 15:59:53 -0500

    we can create a recipe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+    end
+    get '/' do
+        erb :index
+    end
 
-  # code actions here!
+    get '/recipes' do
+        @recipe = Recipe.all
+        erb :recipes
+    end
+
+    get '/recipes/new' do
+        erb :new
+    end
+    post '/recipes' do
+        @new_recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])     
+        erb :show
+    end
+    get '' do
+
+    end
 
 end
